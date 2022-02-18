5bf4fc5bc44415a89ecc11536edc671c1d97298b
IT21204166 <99192717+it21204166@users.noreply.github.com>
2022-02-18 09:13:32 +0000

    convert c to c++

--- tute04.cpp
+++ tute04.cpp
@@ -22,8 +22,26 @@ int main() {
   std::cin >> n;
   std::cout << "Enter a value for r ";
   std::cin >> r;
-  std::cout << "nCr = ";
-  std::cout << nCr(n,r);
-  std::cout << std::endl;
-  return 0;
+	
+std::cout << "nCr = ";
+ std::cout << nCr(n,r);
+ std::cout << std::endl;
+	return 0;
+}
+
+long nCr(int n, int r)//to find factorial of value r
+{
+	
+	return Factorial(n)/(Factorial(r)*Factorial(n-r));
+}
+
+long Factorial(int no)//to find factorial of value n
+{
+	long fac_n=1;
+	for(int i=1;i<=no;i++)
+	{
+		fac_n=fac_n*i;
+	}
+
+	return fac_n;
 }
